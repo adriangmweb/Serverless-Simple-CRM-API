ea75cd21d2b938e153243f14150257c159e93a04
Adrian GM <adriangmweb@gmail.com>
2020-06-16 16:29:57 +0100

    add validateToken and generatePolicy helpers

--- api/auth/helpers/index.js
+++ api/auth/helpers/index.js
@@ -1,7 +1,11 @@
 const comparePassword = require('./comparePassword')
+const generatePolicy = require('./generatePolicy')
 const signToken = require('./signToken')
+const validateToken = require('./validateToken')
 
 module.exports = {
   comparePassword,
+  generatePolicy,
   signToken,
+  validateToken
 }
