9037f2baac9ac178ee77d35efd71afb8022296eb
Adrian GM <adriangmweb@gmail.com>
2020-06-17 17:12:30 +0100

    add customers create, update and remove functionality

--- api/customers/handler.js
+++ api/customers/handler.js
@@ -1,6 +1,6 @@
 'use strict';
 const { errorReporter } = require('../../helpers')
-const { get, list } = require('./actions')
+const { get, list, create, remove, update } = require('./actions')
 
 module.exports.get = async (event) => {
   try {
@@ -32,3 +32,82 @@ module.exports.list = async (event) => {
     return errorReporter(error)
   }
 };
+
+module.exports.create = async (event) => {
+  try {
+    const { principalId } = event.requestContext.authorizer
+
+    const customerParams = {
+      ...JSON.parse(event.body),
+      createdBy: principalId,
+      updatedBy: principalId
+    }
+
+    const newCustomer = await create(customerParams)
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ newCustomer })
+    }
+  } catch (error) {
+    return errorReporter(error)
+  }
+}
+
+module.exports.remove = async (event) => {
+  try {
+    if (!event.body) return errorReporter({ statusCode: 400, response: 'Request body cannot be empty' })
+
+    const { id: customerId = '' } = JSON.parse(event.body)
+    const customerDeleted = await remove(customerId)
+
+    if (!customerId || !customerDeleted) {
+      return errorReporter({
+        statusCode: 404,
+        response: 'Customer not found.'
+      })
+    }
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        customerDeleted: 'Customer deleted successfully'
+      })
+    }
+  } catch (error) {
+    return errorReporter(error)
+  }
+}
+
+module.exports.update = async (event) => {
+  try {
+    if (!event.body) return errorReporter({ statusCode: 400, response: 'Request body cannot be empty' })
+
+    const { principalId } = event.requestContext.authorizer
+
+    const { id: customerId = '', ...attributes } = JSON.parse(event.body)
+
+    const customerAttributes = {
+      ...attributes,
+      updatedBy: principalId
+    }
+
+    const customerUpdated = await update(customerId, customerAttributes)
+
+    if (!customerId || !customerUpdated) {
+      return errorReporter({
+        statusCode: 404,
+        response: 'Customer not found.'
+      })
+    }
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        customerUpdated: 'Customer successfully updated'
+      })
+    }
+  } catch (error) {
+    return errorReporter(error)
+  }
+}
