5d9d8646e6fc1f3d5a767d829e40b1e9baaf85a3
Adrian GM <adriangmweb@gmail.com>
2020-06-18 12:43:20 +0100

    add bucket name and accepted mimeTypes

--- config/index.js
+++ config/index.js
@@ -5,8 +5,13 @@ module.exports = {
     'user',
     'users'
   ],
+  bucketName: `${process.env.NODE_ENV}-crm-api-photo-bucket`,
   database: database,
+  mimeTypes: [
+    'image/jpeg',
+    'image/png'
+  ],
   jwtSecret: process.env.JWT_SECRET_KEY,
-  jwtExpiration: process.env.JWT_EXPIRATION || '1h',
-  seedsPassword: process.env.SEEDS_PASSWORD || 'customPassword'
+  jwtExpiration: process.env.JWT_EXPIRATION || '1h',
+  seedsPassword: process.env.SEEDS_PASSWORD || 'customPassword'
 }
